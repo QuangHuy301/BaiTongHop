241fac3620f4a26f6e1ac09b6192002d37202992
QuangHuy301 <lqhuy301@gmail.com>
2022-10-08 23:49:00 +0700

    first commit

---
+++ giuaky_didong/app/src/main/java/com/lqhuy301/giuaky_didong/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.lqhuy301.giuaky_didong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
